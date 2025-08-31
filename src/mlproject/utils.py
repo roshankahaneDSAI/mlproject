4e103f504f11a03928cc2b26ab68c98e127c8b29
Roshan Kahane <roshankahane09@gmail.com>
2025-08-31 22:27:46 +0530

    Created data ingestion method

---
+++ src/mlproject/utils.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import pandas as pd
+import pymysql 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+host=os.getenv("host")
+user=os.getenv("user")
+password=os.getenv("password")
+db=os.getenv("db")
+
+def read_sql_data():
+    logging.info("Reading sql database started")
+    try:
+        mydb = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=db
+        )
+        logging.info("Connection established successfully")
+        df=pd.read_sql_query('Select * from student', mydb)
+        print(df.head())
+
+        return df
+
+    except Exception as e:
+        raise CustomException(e, sys)
